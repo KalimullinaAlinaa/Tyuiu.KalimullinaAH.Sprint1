54b2270f1213dbddf569af5f6045e650875d5068
Tyuiu.KalimullinaAH@outlook.com <boulevarovaalina@icloud.com>
2023-09-27 12:18:34 +0500

    Task6

---
+++ Tyuiu.KalimullinaAH.Sprint1.Task6.V10.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KalimullinaAH.Sprint1.Task6.V10.Lib;
+namespace Tyuiu.KalimullinaAH.Sprint1.Task6.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            DataService ds = new DataService();
+            string inputText = "Сегодня хорошая погода";
+            string wait = "Сегдня хоршая погода";
+            string res = ds.DeleteMiddleLetter(inputText);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
--- Tyuiu.KalimullinaAH.Sprint1.Task6.V10/Program.cs
+++ Tyuiu.KalimullinaAH.Sprint1.Task6.V10/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.KalimullinaAH.Sprint1.Task5.V1.Lib;
-namespace Tyuiu.KalimullinaAH.Sprint1.Task5.V1
+using Tyuiu.KalimullinaAH.Sprint1.Task6.V10.Lib;
+namespace Tyuiu.KalimullinaAH.Sprint1.Task6.V10
 {
     class Program
     {
@@ -15,34 +15,28 @@ namespace Tyuiu.KalimullinaAH.Sprint1.Task5.V1
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-            Console.WriteLine("* Задание #5                                                              *");
-            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
             Console.WriteLine("* Выполнила: Калимуллина Алина Халимовна | ИИПБ-23-1                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу , которая решает следующую задачу:                   *");
-            Console.WriteLine("* Найти расстояние между двумя точками с заданными координатами x,y       *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст.                          *");
+            Console.WriteLine("* Напечатать все слова, удалив среднюю букву у слов нечетной длины.       *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("Введите координату х1");
-            double x1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите координату х2");
-            double x2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите координату у1");
-            double y1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите координату у2");
-            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите текст");
+             string Text = Convert.ToString(Console.ReadLine());
+            
 
-            var res = ds.DistanceBetweenDots(x1,y1,x2,y2);
-            var result = Convert.ToInt32(res);
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("Расстояние междy точками = " + (res));
+            var res = ds.DeleteMiddleLetter(Text);
 
             Console.ReadKey();
         }
     }
-}
+    }
+
